cdb064f3bba6d5be16060ef1fdd8350b86943562
Kobayashi(attela) <kobayashi@attela.jp>
2017-05-15 13:44:46 +0900

    aslantの修正

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,6 +1,12 @@
 import config from '../config';
 
+/**
+ * [config指定のapiURLをもとにバージョン・パスを含めて返す]
+ * @param  {string} version [APIのバージョン]
+ * @param  {string} path    [APIのパス]
+ * @return {string}         [APIのURL]
+ */
 export default (version, path) => {
   const versionPath = `${version}/${path}`;
   return config.apiUrl + versionPath;
-}
+};
--- src/utils/staticImage.js
+++ src/utils/staticImage.js
@@ -1,4 +1,10 @@
+import config from '../config';
+
+/**
+ * [画像のパスを返す]
+ * @param  {string]} path [画像のファイル名]
+ * @return {string]}      [URLのフルパス]
+ */
 export default (path) => {
-  // FIXME: Use config
-  return `/assets/img/${path}`;
+  return `${config.url}assets/img/${path}`;
 };
